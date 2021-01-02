674a19d47cf516cbca27fb820e3025f2c33b6cb9
Darko Filipovski <darkofilipovski0@gmail.com>
2021-01-02 18:37:30 +0100

    Fixing up missing ID in aggregate root

--- src/main/java/com/filipovski/drboson/sharedkernel/domain/base/AggregateRoot.java
+++ src/main/java/com/filipovski/drboson/sharedkernel/domain/base/AggregateRoot.java
@@ -1,4 +1,10 @@
 package com.filipovski.drboson.sharedkernel.domain.base;
 
 public abstract class AggregateRoot<ID extends DomainObjectId> extends AbstractEntity<ID> {
+
+    public AggregateRoot() {}
+
+    public AggregateRoot(ID id) {
+        super(id);
+    }
 }
