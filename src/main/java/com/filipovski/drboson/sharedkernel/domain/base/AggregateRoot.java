299f41a521c656689dce3f073824be1d875dbb14
Darko Filipovski <darkofilipovski0@gmail.com>
2021-01-05 15:40:01 +0100

    Added logic for registering aggregate domain events

--- src/main/java/com/filipovski/drboson/sharedkernel/domain/base/AggregateRoot.java
+++ src/main/java/com/filipovski/drboson/sharedkernel/domain/base/AggregateRoot.java
@@ -1,10 +1,42 @@
 package com.filipovski.drboson.sharedkernel.domain.base;
 
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.AfterDomainEventPublication;
+import org.springframework.data.domain.DomainEvents;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public abstract class AggregateRoot<ID extends DomainObjectId> extends AbstractEntity<ID> {
+
+    @Transient
+    private transient final List<Object> domainEvents = new ArrayList<>();
 
     public AggregateRoot() {}
 
     public AggregateRoot(ID id) {
         super(id);
     }
+
+    protected <T> T registerEvent(T event) {
+
+        Assert.notNull(event, "Domain event must not be null!");
+
+        this.domainEvents.add(event);
+        return event;
+    }
+
+    @AfterDomainEventPublication
+    protected void clearDomainEvents() {
+        this.domainEvents.clear();
+    }
+
+    @DomainEvents
+    protected Collection<Object> domainEvents() {
+        return Collections.unmodifiableList(domainEvents);
+    }
+
 }
--- src/test/java/com/filipovski/drboson/sharedkernel/SharedKernelApplicationTests.java
+++ src/test/java/com/filipovski/drboson/sharedkernel/SharedKernelApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SharedKernelApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
